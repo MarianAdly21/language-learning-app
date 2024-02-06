277583f23d1eb273661208de14a0743b50fd2f2f
Marian Adly <marianadly2004@gmail.com>
2024-02-06 23:50:53 +0200

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,73 @@ class TokuApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Color(0xffFEF6DB),
         appBar: AppBar(
           title: Text(
             'Toku',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Color(0xff46322B),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity,
+              color: Color(0xffEF9235),
+              child: Text(
+                'Mumbers',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity,
+              color: Color(0xff528031),
+              child: Text(
+                'FamilyMumbers',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity,
+              color: Color(0xff7c3fa1),
+              child: Text(
+                'Colors',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity,
+              color: Color(0xff46a5ca),
+              child: Text(
+                'Phrases',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
