1b6406816fac1a555def5c9bfc83042841368266
Marian Adly <marianadly2004@gmail.com>
2024-02-06 23:15:15 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
